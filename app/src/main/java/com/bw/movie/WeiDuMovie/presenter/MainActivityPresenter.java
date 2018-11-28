2e75675791b7468d89bf65151d7455559198ddba
wisdomli <43641891+wisdomli@users.noreply.github.com>
2018-11-28 11:29:24 +0800

    //改动整体布局

--- app/src/main/java/com/bw/movie/WeiDuMovie/presenter/MainActivityPresenter.java
+++ app/src/main/java/com/bw/movie/WeiDuMovie/presenter/MainActivityPresenter.java
@@ -15,7 +15,9 @@ import android.widget.ImageView;
 
 import com.bw.movie.WeiDuMovie.R;
 import com.bw.movie.WeiDuMovie.activity.MainActivity;
+import com.bw.movie.WeiDuMovie.fragment.CinemaFragment;
 import com.bw.movie.WeiDuMovie.fragment.FilmFragment;
+import com.bw.movie.WeiDuMovie.fragment.MyFragment;
 import com.bw.movie.WeiDuMovie.mvp.view.AppDelegate;
 
 import java.util.ArrayList;
@@ -23,16 +25,17 @@ import java.util.List;
 
 /**
  * 作者:李自强
- * <p>
+ * <整体布局>
  * 2018/11/27
  **/
 public class MainActivityPresenter extends AppDelegate implements View.OnClickListener {
-    private ImageView btn_film,btn_cinema,btn_my;
+    private ImageView btn_film, btn_cinema, btn_my;
     private int wheight;
     private FrameLayout min_frgament;
-   private List<Fragment> fragments = new ArrayList<>();
+    private List<Fragment> fragments = new ArrayList<>();
     private ViewPager min_ViewPager;
     private boolean isClik = true;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_main;
@@ -41,16 +44,20 @@ public class MainActivityPresenter extends AppDelegate implements View.OnClickLi
     @Override
     public void initData() {
         super.initData();
-         btn_film =(ImageView) get(R.id.btn_film);
-         btn_cinema = (ImageView)get(R.id.btn_cinema);
-         btn_my =(ImageView) get(R.id.btn_my);
+        btn_film = (ImageView) get(R.id.btn_film);
+        btn_cinema = (ImageView) get(R.id.btn_cinema);
+        btn_my = (ImageView) get(R.id.btn_my);
 
-        setOnClikLisener(this,R.id.btn_film,R.id.btn_cinema,R.id.btn_my);
+        setOnClikLisener(this, R.id.btn_film, R.id.btn_cinema, R.id.btn_my);
         Display defaultDisplay = ((MainActivity) context).getWindowManager().getDefaultDisplay();
-        wheight =  defaultDisplay.getWidth();
+        wheight = defaultDisplay.getWidth();
         FragmentManager manager = ((MainActivity) context).getSupportFragmentManager();
         FragmentPager fragmentPager = new FragmentPager(manager);
-         min_ViewPager =(ViewPager) get(R.id.min_ViewPager);
+        min_ViewPager = (ViewPager) get(R.id.min_ViewPager);
+
+        fragments.add(new FilmFragment());
+        fragments.add(new CinemaFragment());
+        fragments.add(new MyFragment());
         min_ViewPager.setAdapter(fragmentPager);
 
         setSizeMax(btn_film);
@@ -62,56 +69,56 @@ public class MainActivityPresenter extends AppDelegate implements View.OnClickLi
         min_ViewPager.setCurrentItem(0);
     }
 
-     private class FragmentPager extends FragmentPagerAdapter {
-         public FragmentPager(FragmentManager fm) {
-             super(fm);
-         }
+    private class FragmentPager extends FragmentPagerAdapter {
+        public FragmentPager(FragmentManager fm) {
+            super(fm);
+        }
 
-         @Override
-         public Fragment getItem(int i) {
-             return fragments.get(i);
-         }
+        @Override
+        public Fragment getItem(int i) {
+            return fragments.get(i);
+        }
 
-         @Override
-         public int getCount() {
-             return fragments.size();
-         }
-     }
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
 
 
     private Context context;
+
     @Override
     public void getContext(Context context) {
         super.getContext(context);
         this.context = context;
     }
 
-    public void setSizeMax(ImageView imageView){
+    public void setSizeMax(ImageView imageView) {
         ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
-        layoutParams.height = 70 *(wheight / 2) / 160;
-        layoutParams.width = 70 *(wheight / 2) / 160;
+        layoutParams.height = 70 * (wheight / 2) / 160;
+        layoutParams.width = 70 * (wheight / 2) / 160;
     }
 
-    public void setSizeMin(ImageView imageView){
+    public void setSizeMin(ImageView imageView) {
         ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
-        layoutParams.height = 60 *(wheight / 2) / 160;
-        layoutParams.width = 60 *(wheight / 2) / 160;
+        layoutParams.height = 60 * (wheight / 2) / 160;
+        layoutParams.width = 60 * (wheight / 2) / 160;
     }
 
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_film:
 
-                    setSizeMax(btn_film);
-                    setSizeMin(btn_cinema);
-                    setSizeMin(btn_my);
-                    btn_film.setImageResource(R.drawable.com_icon_film_selected);
-                    btn_cinema.setImageResource(R.drawable.com_icon_cinema_default);
-                    btn_my.setImageResource(R.drawable.com_icon_my_default);
-                    min_ViewPager.setCurrentItem(0);
-
+                setSizeMax(btn_film);
+                setSizeMin(btn_cinema);
+                setSizeMin(btn_my);
+                btn_film.setImageResource(R.drawable.com_icon_film_selected);
+                btn_cinema.setImageResource(R.drawable.com_icon_cinema_default);
+                btn_my.setImageResource(R.drawable.com_icon_my_default);
+                min_ViewPager.setCurrentItem(0);
 
 
                 break;
@@ -134,7 +141,5 @@ public class MainActivityPresenter extends AppDelegate implements View.OnClickLi
                 min_ViewPager.setCurrentItem(2);
                 break;
         }
-
-
     }
 }
