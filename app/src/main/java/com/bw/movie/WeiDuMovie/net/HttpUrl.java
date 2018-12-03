762abd65597f115686da1e4b38b1b9e8401a9aa0
WenHao168 <97979131@qq.com>
2018-12-03 10:29:25 +0800

    温浩1222

--- app/src/main/java/com/bw/movie/WeiDuMovie/net/HttpUrl.java
+++ app/src/main/java/com/bw/movie/WeiDuMovie/net/HttpUrl.java
@@ -15,4 +15,5 @@ public class HttpUrl {
     public static String CinemasUrl = "movieApi/cinema/v1/findRecommendCinemas";
     public static String FeedBackUrl = "movieApi/tool/v1/verify/recordFeedBack";
     public static String MoviesDetailUrl ="movieApi/movie/v1/findMoviesDetail";
+    public static String UpdateUrl ="movieApi/user/v1/verify/modifyUserInfo";
 }
